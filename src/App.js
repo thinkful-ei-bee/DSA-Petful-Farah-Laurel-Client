8ba4dcc392e3dfca2198367ba7ddb022960bf861
Farah Student <farah.gus@hotmail.com>
2019-05-24 14:51:13 -0400

    fetch api working

--- src/App.js
+++ src/App.js
@@ -4,42 +4,41 @@ import { Route, withRouter } from 'react-router-dom';
 import MainPage from './Components/MainPage.js';
 import AdoptionPage from './Components/AdoptionPage.js';
 
-function App() {
- 
+class App extends React.Component {
   state = {
     results: [],
   }
 
-
-  getDogs = () => {
-    return fetch(`https://localhost:8080/api/dog`, {
-      method: 'GET',
-      headers: {
-        'content-type': 'application/json'
-      }
-    })
-    .then(res => {
-      return res.json()
-    })
-    .then(data => {
-      this.setState({
-        results: data
-      })
-      console.log(this.state.results)
-    })
-    .catch(error => console.log('Error', error))
-  };
-
   componentDidMount() {
-    this.getDogs();
+    this.fetchApi('dog', 'dog');
   }
 
-  return (
+  fetchApi(endpoint, statekey, method = 'GET', apiBody) {
+    fetch(`http://localhost:8000/api/${endpoint}`, {
+      method: method,
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(apiBody)
+    })
+    .then(response => {
+      console.log(response)
+      return response.json()
+    })
+    .then(response => {
+      console.log(response)
+      this.setState({ [statekey]: response} );
+    })
+  }
+
+  render() {
+    return (
     <div className="App">
         <Route exact path="/" component={MainPage}/>
         <Route path="/adoptions" component={AdoptionPage} />
     </div>
   );
+  }
 }
 
 export default withRouter(App);
