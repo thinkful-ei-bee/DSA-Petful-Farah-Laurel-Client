25cb4b96d2c47790a7d778451d062f1152fd612f
Farah Student <farah.gus@hotmail.com>
2019-05-24 15:15:15 -0400

    dog pictures up but not in queue

--- src/Components/dog.js
+++ src/Components/dog.js
@@ -2,10 +2,23 @@ import React from 'react';
 
 class Dog extends React.Component {
   render() {
+    const dogs = this.props.dogs;
+    const dogInfo = dogs.map((dog, i) => 
+      <div key={i}>
+        <img src={dog.imageURL} alt={dog.imageDescription}/>
+        <h3>More about: {dog.name}</h3>
+        <p>Sex: {dog.sex}</p>
+        <p>Age: {dog.age}</p>
+        <p>Breed: {dog.breed}</p>
+        <p>Story: {dog.story}</p>
+      </div>
+      )
+
     console.log('dogs:', this.props.dogs)
     return(
       <div className="dog">
           <h1>I love dogs!</h1>
+          {dogInfo}
       </div>
     )
   }
